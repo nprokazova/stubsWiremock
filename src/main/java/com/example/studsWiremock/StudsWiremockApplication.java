ef2f70fec8313c793f47460a24b79419e93cbaf1
nprokazova <naprokazova@gmail.com>
2021-08-01 20:18:22 +0300

    lesson23

---
+++ src/main/java/com/example/studsWiremock/StudsWiremockApplication.java
@@ -0,0 +1,13 @@
+package com.example.studsWiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudsWiremockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudsWiremockApplication.class, args);
+	}
+
+}
